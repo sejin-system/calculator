ef3521bd96c2b2b9e8da5b1c9207c9297b6eb447
Sejin System <sejin.system.kr@gmail.com>
2020-05-30 20:58:46 +0900

    [sogong] modify main.c -> (execute) ./cal 3 4

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "cal.h"
+
 
 int main(int argc, char* argv[])
 {
-	printf("%f + %f = %f\n", 3, 3, 6);
-	SJADD(1,2);
+	float a, b, result = 0;
+
+	a = atof(argv[1]);
+	b = atof(argv[2]);
+	
+	result = SJADD(a, b);
+	printf("%f + %f = %f\n", a, b, result);
+	
+	result = SJSUB(a, b);
+	printf("%f - %f = %f\n", a, b, result);
+	
+	result = SJMUL(a, b);
+	printf("%f * %f = %f\n", a, b, result);
+	
+	result = SJDIV(a, b);
+	printf("%f / %f = %f\n", a, b, result);
+	
 	return 0;
 
 }
